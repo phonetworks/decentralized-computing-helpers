91539f724e032bd4e799e82155e263ad511df8a8
Emre <esokullu@gmail.com>
2019-07-28 11:42:26 +0300

    simplified

--- src/Pho/DecentralizedComputing/Helpers.php
+++ src/Pho/DecentralizedComputing/Helpers.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 /*
  * This file is part of the Pho package.
@@ -11,6 +11,8 @@
 
 namespace Pho\DecentralizedComputing;
 
+use BCMathExtended\BC;
+
 /**
  * This class contains helper methods for decentralized
  * computing in PHP.
@@ -22,97 +24,64 @@ namespace Pho\DecentralizedComputing;
 class Helpers
 {
 
-    const HEXADECIMALS = "0123456789ABCDEF";
-    const DECIMALS = "0123456789";
+    /**
+     * Generates a cryptographically secure random Id
+     * 
+     * Completely random
+     * better for security
+     * To use as key and node ID
+     * 160 bits, e.g. 20 bytes
+     *
+     * @return string
+     */
+    public static function generate_kademlia_id(): string
+    {
+        //return \sha1(\random_bytes(16));
+        return  \bin2hex(\random_bytes(20));
+    }
 
     /**
      * Computes XOR distance between two numbers.
+     * 
+     * Parameters in string format to deal with bignums
      *
      * @link https://stackoverflow.com/questions/27081124/binary-separation-distance-java
      * 
-     * @param int $num1
-     * @param int $num2 
+     * @param string $num1 in decimal format
+     * @param string $num2 in decimal format
      * 
-     * @return int The distance
+     * @return int The distance in decimal format
      */
-    public static function xor_distance(int $num1, int $num2): int
+    public static function xor_distance(string $num1, string $num2): int
     {
-         $count = 0;
-         $xor = $num1 xor $num2;
+        $count = 0;
+        $xor = BC::bitXor($num1, $num2);
         while($xor!=0) {
             $count++;
-            $xor &= ($xor-1);
+            $xor = BC::bitXor($xor, bcsub($xor, 1));  //$xor &= ($xor-1);
         }
-         return $count;
-    }
-
-    /**
-     * Computes XOR distance between two strings.
-     *
-     * @see xor_distance
-     * 
-     * @param string $string1
-     * @param string $string2 
-     * 
-     * @return int The distance
-     */
-    public static function string_xor_distance(string $string1, string $string2): int
-    {
-         $num1 = convert_base_for_bignum(sha1($string1), self::HEXADECIMALS, self::DECIMALS);
-         $num2 = convert_base_for_bignum(sha1($string2), self::HEXADECIMALS, self::DECIMALS);
-         return self::xor_distance($num1, $num2);
+        return $count;
     }
 
 
-
-    /**
-     * Like PHP's native base_convert function
-     * but works for bignumbers.
-     * 
-     * @link https://gist.github.com/macik/4758146
-     * 
-     * @param string $numberInput
-     * @param string $fromBaseInput
-     * @param string $toBaseInput
-     * 
-     * @return string
-     */
-    public static function convert_base_for_bignum(string $numberInput, string $fromBaseInput, string $toBaseInput): string
+    public static function hex_to_bin(string $hex): string
     {
-        if ($fromBaseInput==$toBaseInput) { 
-            return $numberInput;
-        }
+        // https://stackoverflow.com/a/25017999
+        $dec2bin = function ($decimal_i)
+        {
+            \bcscale(0);
+            $binary_i = '';
+            do
+            {
+            $binary_i = \bcmod($decimal_i,'2') . $binary_i;
+            $decimal_i = \bcdiv($decimal_i,'2');
+            } while (\bccomp($decimal_i,'0'));
 
-         $fromBase = str_split($fromBaseInput, 1);
-         $toBase = str_split($toBaseInput, 1);
-         $number = str_split($numberInput, 1);
-         $fromLen=strlen($fromBaseInput);
-         $toLen=strlen($toBaseInput);
-         $numberLen=strlen($numberInput);
-         $retval='';
-
-        if ($toBaseInput == '0123456789') {
-            $retval=0;
-            for ($i = 1;$i <= $numberLen; $i++) {
-                $retval = bcadd($retval, bcmul(array_search($number[$i-1], $fromBase), bcpow($fromLen, $numberLen-$i)));
-            }
-            return $retval;
-        }
-     
-        if ($fromBaseInput != '0123456789') {
-            $base10=convBase($numberInput, $fromBaseInput, '0123456789');
-        } else {
-                 $base10 = $numberInput;
-        }
-        if ($base10<strlen($toBaseInput)) {
-            return $toBase[$base10];
-        }
-        while($base10 != '0')
-         {
-            $retval = $toBase[bcmod($base10, $toLen)].$retval;
-            $base10 = bcdiv($base10, $toLen, 0);
-        }
-         return $retval;
+            return($binary_i);
+        };
+        return $dec2bin(
+            BC::hexdec($hex)
+        );
     }
 
 
